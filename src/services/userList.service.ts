ce74079ccddf9e436a402cfcae10439194c1c2ad
GabrielRD <grd342@outlook.com>
2022-05-13 20:01:26 -0300

    fix: user_list

--- src/services/userList.service.ts
+++ src/services/userList.service.ts
@@ -4,9 +4,19 @@ import { User } from "../entities/user.entity";
 const userListService = async () => {
   const userRepository = AppDataSource.getRepository(User);
 
-  const users = userRepository.find();
+  const users: any = await userRepository.find();
 
-  return users;
+  let newList: any = [];
+
+  users.forEach((element: any) => {
+    const {
+      password,
+      ...noPasswordUser
+    }: { password: string; noPasswordUser: any } = element;
+
+    newList.push(noPasswordUser);
+  });
+  return newList;
 };
 
 export default userListService;
